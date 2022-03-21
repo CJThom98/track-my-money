416b8b6cc25526e3939d8b9ae5d3b9e8523df3fb
Cory Thompson <cory.thompson98@gmail.com>
2022-03-21 10:58:24 -0500

    success

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,3 +7,12 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_budget', { autoIncrement: true });
 };
+
+// upon a success
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
