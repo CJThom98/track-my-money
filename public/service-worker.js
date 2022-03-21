436ecacc193b109d5d298e435616f6fbd0fd271b
Cory Thompson <cory.thompson98@gmail.com>
2022-03-21 11:47:15 -0500

    files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/js/index.js',
+    '/js/idb.js',
+    '/css/style.css',
+    '/icon/icon-72x72.png',
+    '/icon/icon-96x96.png',
+    '/icon/icon-128x128.png',
+    '/icon/icon-144x144.png',
+    '/icon/icon-152x152.png',
+    '/icon/icon-384x384.png',
+    '/icon/icon-512x512.png'
+];
+
+
+self.addEventListener('install', function (e) {
+    
+})
